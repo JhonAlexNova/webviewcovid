2062d7c83dac98b34e963d7b4b6664d329846511
jimmy sorza <28790876+JimmySorza@users.noreply.github.com>
2020-07-05 15:15:31 -0500

    Initial Commit

---
+++ ios-webcorpovirus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-webcorpovirus
+//
+//  Created by jimmy sorza on 05/07/20.
+//  Copyright © 2020 jimmy sorza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
